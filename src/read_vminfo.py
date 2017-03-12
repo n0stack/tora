293ce5a0e915f92c64188ea8bbe09248331bd60f
palloc <palloc.mknet10@gmail.com>
2017-03-12 17:30:47 +0900

    fix some

--- src/read_vminfo.py
+++ src/read_vminfo.py
@@ -4,7 +4,7 @@ import libvirt
 import os
 import re
 from xml.dom import minidom
-from .Base import BaseFunc
+from Base import BaseFunc
 QEMU_URL = "qemu:///system"
 
 
@@ -14,12 +14,38 @@ class VMInfo(BaseFunc):
         super().__init__()
 
 
+    # get interface's information
+    def get_domain_network_info(self, iface):
+        iface_type = iface.getAttribute("type")
+        
+        if iface_type == "network":
+            network_info = iface.getElementsByTagName("source")[0].getAttribute("network")
+            net_XML_info = minidom.parseString(Conn.networkLookupByName(network_info).XMLDesc(0))
+            bridge = net_XML_info.getElementsByTagName("bridge")[ 0 ].getAttribute("name")
+            mac_address = ""
+            device = ""
+
+        elif iface_type == "bridge":
+            network_info = ""
+            bridge = iface.getElementsByTagName("source")[0].getAttribute("bridge")
+            mac_address = iface.getElementsByTagName("mac")[0].getAttribute("address")
+            device = iface.getElementsByTagName("target")[0].getAttribute("dev")
+            
+        print ("*"*20)
+        print ("Network device = {}".format(device))
+        print ("Bridge = {}".format(bridge))
+        print ("Mac address = {}".format(mac_address))
+        print ("Network information = {}".format(network_info))
+        
+
     # Show all domain's all information(id,name,state...)
     def show_domain_info_all(self):
-        print ("-"*30)
         for id in self.connection.listDomainsID():
+            print ("-"*30)
+
             domain = self.connection.lookupByID(id)
             info = domain.info()
+
             print ("ID = {}".format(id))
             print ("Name = {}".format(domain.name()))
             print ("State = {}".format(info[0]))
@@ -30,31 +56,12 @@ class VMInfo(BaseFunc):
             # Read XML file
             domain_XML = minidom.parseString(domain.XMLDesc(0))
 
-            # print interface's information
+            # Show interface's information
             for iface in domain_XML.getElementsByTagName("interface"):
-                iface_type = iface.getAttribute("type")
-
-                if iface_type == "network":
-                    network_info = iface.getElementsByTagName("source")[0].getAttribute("network")
-                    net_XML_info = minidom.parseString(Conn.networkLookupByName(network_info).XMLDesc(0))
-                    bridge = net_XML_info.getElementsByTagName("bridge")[ 0 ].getAttribute("name")
-                    mac_address = ""
-                    device = ""
-
-                elif iface_type == "bridge":
-                    network_info = ""
-                    bridge = iface.getElementsByTagName("source")[0].getAttribute("bridge")
-                    mac_address = iface.getElementsByTagName("mac")[0].getAttribute("address")
-                    device = iface.getElementsByTagName("target")[0].getAttribute("dev")
-
-                print ("*"*20)
-                print ("Network device = {}".format(device))
-                print ("Bridge = {}".format(bridge))
-                print ("Mac address = {}".format(mac_address))
-                print ("Network information = {}".format(network_info))
+                self.get_domain_network_info(iface)
 
             print ("*"*20)
-                
+        print ("-"*30)                
 
 
 
