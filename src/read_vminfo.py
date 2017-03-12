5590a1ba7f739f754f22f669e2fd5ef1425d7095
palloc <palloc.mknet10@gmail.com>
2017-03-12 12:16:34 +0900

    add function of show domain information

--- src/read_vminfo.py
+++ src/read_vminfo.py
@@ -13,15 +13,36 @@ class VMInfo:
 
 
     def connect_to_qemu(self):
-        conn = libvirt.openReadOnly(QEMU_URL)
+        self.connection = libvirt.openReadOnly(QEMU_URL)
 
-        if conn == None:
-            print 'Failed to connection to the hypervisor'
+        if self.connection == None:
+            print ("Failed to connect to the hypervisor")
             sys.exit(1)
+
+        print ("Success to connect to the hypervisor")
 
 
     def show_domain_all(self):
-        domains = conn.listDefinedDomains()
+        domains = self.connection.listDefinedDomains()
         print (domains)
 
-        
+
+    # Show all domain's all information(id,name,state...)
+    def show_domain_info_all(self):
+        for id in self.connection.listDomainsID():
+            domain = self.connection.lookupByID(id)
+            info = domain.info()
+            print ("ID = {}".format(id))
+            print ("Name = {}".format(domain.name()))
+            print ("State = {}".format(info[0]))
+            print ("Max Memory = {}".format(info[1]))
+            print ("Number of CPUs = {}".format(info[3]))
+            print ("CPU time = {}".format(info[2]))            
+
+
+
+
+# for debug
+vm = VMInfo()
+vm.connect_to_qemu()
+vm.show_domain_info_all()
