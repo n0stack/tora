ca1740747062d2a31af0cd17f676f3dfa63ffad6
palloc <palloc.mknet10@gmail.com>
2017-03-16 14:40:38 +0900

    aaa

--- src/vm_info.py
+++ src/vm_info.py
@@ -16,21 +16,22 @@ class DomainInfo(BaseReadOnly):
     # Bridge interface's name
     def get_bridge(self, iface):
         iface_type = iface.getAttribute("type")
+
         if iface_type == "network":
             net_XML_info = minidom.parseString(Conn.networkLookupByName(network_info).XMLDesc(0))
             bridge = net_XML_info.getElementsByTagName("bridge")[0].getAttribute("name")
-
         elif iface_type == "bridge":
             bridge = iface.getElementsByTagName("source")[0].getAttribute("bridge")
-
         else:
             bridge = None
 
         return bridge
 
+
     # Network name
     def get_network(self, iface):
         iface_type = iface.getAttribute("type")
+
         if iface_type == "network":
             network = iface.getElementsByTagName("source")[0].getAttribute("network")
         else:
@@ -38,18 +39,22 @@ class DomainInfo(BaseReadOnly):
 
         return network
 
+
     # Mac address
     def get_mac(self, iface):
         return iface.getElementsByTagName("mac")[0].getAttribute("address")
+
 
     # Device(nic) name
     def get_device(self, iface):
         return iface.getElementsByTagName("target")[0].getAttribute("dev")
 
+
     # Domain(VM) name
     def get_domain(self, id):
         domain = self.connection.lookupByID(id)
         return domain.name()
+
 
     # VM tatus
     def get_state(self, id):
@@ -62,15 +67,18 @@ class DomainInfo(BaseReadOnly):
         domain = self.connection.lookupByID(id)
         return domain.info()[1]
 
+
     # Number of CPU
     def get_CPU_number(self, id):
         domain = self.connection.lookupByID(id)
         return domain.info()[3]
 
+
     # CPU time
     def get_CPU_time(self, id):
         domain = self.connection.lookupByID(id)
         return domain.info()[2]
+
 
     # Domain's XML(settings)
     def get_domain_XML(self, id):
@@ -100,6 +108,7 @@ class DomainInfo(BaseReadOnly):
             print ("*"*20)
         print ("-"*30)                
 
+
     # for debug
     def get_domain_network_info(self, iface):
         print ("*"*20)
@@ -107,8 +116,6 @@ class DomainInfo(BaseReadOnly):
         print ("Bridge = {}".format(self.get_bridge(iface)))
         print ("Mac address = {}".format(self.get_mac(iface)))
         print ("Device = {}".format(self.get_device(iface)))
-
-
 
 
 # for debug
