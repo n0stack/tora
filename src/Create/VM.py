af3560738a0505eda9af7db27020b6b93968f068
palloc <palloc.mknet10@gmail.com>
2017-05-07 18:33:18 +0900

    add some

--- src/Create/VM.py
+++ src/Create/VM.py
@@ -29,32 +29,36 @@ class CreateVM(BaseOpen):
 
         
     def create_vm(self):
-        boot = "test2.img"
-        cdrom = "/home/palloc/iso_file/mini.iso"
-        vm_name = "test2"
+        boot = "cent1.img"
+        cdrom = "/home/palloc/iso_file/CentOS-7-x86_64-Minimal-1611.iso"
+        vm_name = "cent1"
+
+
         self.getXML()
         #vm = self.connection.lookupByName(vm_name)
 
+        # domain tag
         domain = Element('domain', attrib={'type':'qemu'})
-
+        # name tag
         name = Element('name')
         name.text = vm_name
-
+        # uuid tag
         uuid = Element('uuid')
         uuid.text = utils.randomUUID()
-
+        # description tag
         description = Element('description')
         description.text = "test description"
-
+        # memory tag
         memory = Element('memory', attrib={'unit': 'KiB'})
         memory.text = "256"
-
+        # current memory tag
         currentMemory = Element('currentMemory', attrib={'unit': 'KiB'})
         currentMemory.text = "256"
-        
+        # vcpu tag
         vcpu = Element('vcpu', attrib={'placement': 'static'})
         vcpu.text = "1"
 
+        # Create XML file and append some tags
         devices = self.devices(boot, cdrom)
         domain.append(name)
         domain.append(uuid)
@@ -66,8 +70,10 @@ class CreateVM(BaseOpen):
         domain.append(devices)
 
         xml = ET.tostring(domain).decode('utf-8').replace('\n', '')
-        dom = self.connection.createXML(xml, 0)
-        dom = self.connection.defineXML(xml)
+        print (xml)
+        #dom = self.connection.createXML(xml, 0)
+        #dom = self.connection.defineXML(xml)
+
 
         
         
@@ -75,77 +81,48 @@ class CreateVM(BaseOpen):
     def devices(self, boot, cdrom):
         text = """
         <devices>
-        <emulator>/usr/bin/kvm</emulator>
-        <disk device="disk" type="file">
-        <driver name="qemu" type="raw" />
-        <source file="%s" />
-        <target bus="ide" dev="hda" />
-        <address bus="0" controller="0" target="0" type="drive" unit="0" />
+        <emulator>/usr/bin/qemu-system-x86_64</emulator>
+        <disk type='file' device='disk'>
+        <driver name='qemu' type='qcow2'/>
+        <source file='%s'/>
+        <target dev='hda' bus='ide'/>
+        <address type='drive' controller='0' bus='0' target='0' unit='0'/>
         </disk>
-        <disk device="cdrom" type="file">
-        <driver name="qemu" type="raw" />
-        <source file="%s" />
-        <target bus="ide" dev="hdb" />
-        <readonly />
-        <address bus="0" controller="0" target="0" type="drive" unit="1" />
+        <disk type='file' device='cdrom'>
+        <source file='%s'>
+        <driver name='qemu' type='raw'/>
+        <target dev='hdb' bus='ide'/>
+        <readonly/>
+        <address type='drive' controller='0' bus='0' target='0' unit='1'/>
         </disk>
-        <controller index="0" model="ich9-ehci1" type="usb">
-        <address bus="0x00" domain="0x0000" function="0x7" slot="0x06" type="pci" />
+        <controller type='usb' index='0' model='ich9-ehci1'>
+        <address type='pci' domain='0x0000' bus='0x00' slot='0x05' function='0x7'/>
         </controller>
-        <controller index="0" model="ich9-uhci1" type="usb">
-        <master startport="0" />
-        <address bus="0x00" domain="0x0000" function="0x0" multifunction="on" slot="0x06" type="pci" />
-        </controller>
-        <controller index="0" model="ich9-uhci2" type="usb">
-        <master startport="2" />
-        <address bus="0x00" domain="0x0000" function="0x1" slot="0x06" type="pci" />
-        </controller>
-        <controller index="0" model="ich9-uhci3" type="usb">
-        <master startport="4" />
-        <address bus="0x00" domain="0x0000" function="0x2" slot="0x06" type="pci" />
-        </controller>
-        <controller index="0" model="pci-root" type="pci" />
-        <controller index="0" type="ide">
-        <address bus="0x00" domain="0x0000" function="0x1" slot="0x01" type="pci" />
-        </controller>
-        <controller index="0" type="virtio-serial">
-        <address bus="0x00" domain="0x0000" function="0x0" slot="0x05" type="pci" />
-        </controller>
-        <interface type="network">
-        <mac address="52:54:00:e1:fb:d5" />
-        <source network="default" />
-        <model type="rtl8139" />
-        <address bus="0x00" domain="0x0000" function="0x0" slot="0x03" type="pci" />
+        <interface type='bridge'>
+        <mac address='52:54:00:93:1e:ee'/>
+        <source bridge='br0'/>
+        <model type='rtl8139'/>
+        <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0' />
         </interface>
-        <serial type="pty">
-        <target port="0" />
+        <serial type='pty'>
+        <target port='0'/>
         </serial>
-        <console type="pty">
-        <target port="0" type="serial" />
+        <console type='pty'>
+        <target type='serial' port='0'/>
         </console>
-        <channel type="spicevmc">
-        <target name="com.redhat.spice.0" type="virtio" />
-        <address bus="0" controller="0" port="1" type="virtio-serial" />
-        </channel>
-        <input bus="ps2" type="mouse" />
-        <input bus="ps2" type="keyboard" />
-        <graphics autoport="yes" type="spice">
-        <image compression="off" />
+        <input type='mouse' bus='ps2'/>
+        <input type='keyboard' bus='ps2'/>
+        <graphics type='vnc' port='-1' autoport='yes' listen='0.0.0.0' passwd='vnc'>
+        <listen type='address' address='0.0.0.0'/>
         </graphics>
-        <sound model="ich6">
-        <address bus="0x00" domain="0x0000" function="0x0" slot="0x04" type="pci" />
-        </sound>
         <video>
-        <model heads="1" ram="65536" type="qxl" vgamem="16384" vram="65536" />
-        <address bus="0x00" domain="0x0000" function="0x0" slot="0x02" type="pci" />
+        <model type='cirrus' vram='16384' heads='1'/>
+        <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x0'/>
         </video>
-        <redirdev bus="usb" type="spicevmc"></redirdev>
-        <redirdev bus="usb" type="spicevmc"></redirdev>
-        <memballoon model="virtio">
-        <address bus="0x00" domain="0x0000" function="0x0" slot="0x07" type="pci" />
+        <memballoon model='virtio'>
+        <address type='pci' domain='0x0000' bus='0x00' slot='0x04' function='0x0'/>
         </memballoon>
         </devices>
         """ % (boot, cdrom)
-        
         return ET.XML(text)
 
