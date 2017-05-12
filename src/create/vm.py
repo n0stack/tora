56c8040c847d74c4ac48669f5cdc6f6aedd90082
palloc <palloc.mknet10@gmail.com>
2017-05-12 14:05:46 +0900

    refactoring

--- src/create/vm.py
+++ src/create/vm.py
@@ -14,11 +14,12 @@ class CreateVM(BaseOpen):
     def __init__(self):
         super().__init__()
 
-    def __call__(self, name, boot, cdrom, memory):
+    def __call__(self, name, boot, cdrom, memory_size, vcpu_num):
         self.vm_name = name
         self.boot = boot
         self.cdrom = cdrom
-        self.memory = memory 
+        self.memory_size = memory_size
+        self.vcpu_num = vcpu_num
 
         # Check the existence of VM
         try:
@@ -31,9 +32,6 @@ class CreateVM(BaseOpen):
 
         # domain tag
         domain = Element('domain', attrib={'type': 'qemu'})
-
-        # os tag
-        self.create_os_tag()
 
         # name tag
         name = Element('name')
@@ -49,15 +47,18 @@ class CreateVM(BaseOpen):
 
         # memory tag
         memory = Element('memory', attrib={'unit': 'KiB'})
-        memory.text = "256"
+        memory.text = str(self.memory_size)
 
         # current memory tag
         currentMemory = Element('currentMemory', attrib={'unit': 'KiB'})
-        currentMemory.text = "256"
+        currentMemory.text = str(self.memory_size)
 
         # vcpu tag
         vcpu = Element('vcpu', attrib={'placement': 'static'})
-        vcpu.text = "1"
+        vcpu.text = str(self.vcpu_num)
+
+        # os tag
+        self.create_os_tag()
 
         # devices tag
         self.create_devices_tag()
@@ -77,6 +78,8 @@ class CreateVM(BaseOpen):
         # dom = self.connection.createXML(xml, 0)
         # dom = self.connection.defineXML(xml)
 
+
+
     def create_os_tag(self):
         self.os = Element('os')
 
@@ -95,43 +98,17 @@ class CreateVM(BaseOpen):
         self.devices = Element('devices')
 
         # emulator tag
-        emulator = Element('emulator')
-        emulator.text = "/usr/bin/qemu-system"
+        self.emulator = Element('emulator')
+        self.emulator.text = "/usr/bin/qemu-system"
 
         # disk1 tag
-        disk1 = Element('disk', attrib={'type': 'file', 'device': 'disk'})
-        driver = Element('driver', attrib={'name': 'qemu', 'type': 'qcow2'})
-        source = Element('source', attrib={'file': self.boot})
-        target = Element('target', attrib={'dev': 'vda', 'bus': 'virtio'})
-        address = Element('address', attrib={'type': 'pci',
-                                             'domain': '0x0000',
-                                             'bus': '0x00',
-                                             'slot': '0x06',
-                                             'function': '0x0'})
-        disk1.append(driver)
-        disk1.append(source)
-        disk1.append(target)
-        disk1.append(address)
+        self.create_qcow2_tag()
 
         # disk2 tag(cdrom)
-        disk2 = Element('disk', attrib={'type': 'file', 'device': 'cdrom'})
-        driver = Element('driver', attrib={'name':'qemu', 'type': 'raw'})
-        source = Element('source', attrib={'file': self.cdrom})
-        target = Element('target', attrib={'bus': 'ide', 'dev': 'hdb'})
-        read_only = Element('readonly')
-        address = Element('address', attrib={'bus': '0',
-                                             'controller': '0',
-                                             'target': '0',
-                                             'type': 'drive',
-                                             'unit': '1'})
-        disk2.append(driver)
-        disk2.append(source)
-        disk2.append(target)
-        disk2.append(read_only)
-        disk2.append(address)
-                                                        
+        self.create_iso_tag()
+
         # controller1 tag
-        controller1 = Element('controller', attrib={'type': 'usb',
+        self.controller1 = Element('controller', attrib={'type': 'usb',
                                                     'index': '0',
                                                     'model': 'ich9-ehci1'})
         address = Element('address', attrib={'type': 'pci',
@@ -139,10 +116,10 @@ class CreateVM(BaseOpen):
                                              'bus': '0x00',
                                              'slot': '0x05',
                                              'function': '0x7'})
-        controller1.append(address)
+        self.controller1.append(address)
 
         # controller2 tag
-        controller2 = Element('controller', attrib={'type': 'usb',
+        self.controller2 = Element('controller', attrib={'type': 'usb',
                                                     'index': '0',
                                                     'model': 'ich9-uhci1'})
         master = Element('master', attrib={'startport': '0'})
@@ -152,11 +129,11 @@ class CreateVM(BaseOpen):
                                              'slot': '0x05',
                                              'function': '0x0',
                                              'multifunction': 'on'})
-        controller2.append(master)
-        controller2.append(address)
+        self.controller2.append(master)
+        self.controller2.append(address)
 
         # interface1 tag
-        interface1 = Element('interface', attrib={'type': 'bridge'})
+        self.interface1 = Element('interface', attrib={'type': 'bridge'})
         mac = Element('mac', attrib={'address': '52:54:00:43:e0:a0'})
         source = Element('source', attrib={'bridge': 'br0'})
         model = Element('model', attrib={'type': 'virtio'})
@@ -165,23 +142,24 @@ class CreateVM(BaseOpen):
                                              'bus': '0x00',
                                              'slot': '0x02',
                                              'function': '0x0'})
-        interface1.append(mac)
-        interface1.append(source)
-        interface1.append(model)
-        interface1.append(address)
+        self.interface1.append(mac)
+        self.interface1.append(source)
+        self.interface1.append(model)
+        self.interface1.append(address)
 
         # serial tag
-        serial = Element('serial', attrib={'type': 'pty'})
+        self.serial = Element('serial', attrib={'type': 'pty'})
         target = Element('target', attrib={'port': '0'})
-        serial.append(target)
+        self.serial.append(target)
 
         # console tag
-        console = Element('console', attrib={'type': 'pty'})
+        self.console = Element('console', attrib={'type': 'pty'})
         target = Element('target', attrib={'type': 'serial',
                                            'port': '0'})
+        self.console.append(target)
 
         # channel tag
-        channel = Element('channel', attrib={'type': 'unix'})
+        self.channel = Element('channel', attrib={'type': 'unix'})
         source = Element('source', attrib={'mode': 'bind'})
         target = Element('target', attrib={'type': 'virtio',
                                            'name': 'org.qemu.guest_agent.0'})
@@ -189,31 +167,65 @@ class CreateVM(BaseOpen):
                                              'controller': '0',
                                              'bus': '0',
                                              'port': '1'})
-        channel.append(source)
-        channel.append(target)
-        channel.append(address)
+        self.channel.append(source)
+        self.channel.append(target)
+        self.channel.append(address)
 
         # input tag
-        input = Element('input', attrib={'type': 'tablet',
+        self.input = Element('input', attrib={'type': 'tablet',
                                          'bus': 'usb'})
 
         # memballoon tag
-        memballoon = Element('memballoon', attrib={'model': 'virtio'})
+        self.memballoon = Element('memballoon', attrib={'model': 'virtio'})
         address = Element('address', attrib={'type': 'pci',
                                              'domain': '0x0000',
                                              'bus': '0x00',
                                              'slot': '0x07',
                                              'function': '0x0'})
-        memballoon.append(address)
+        self.memballoon.append(address)
 
-        self.devices.append(emulator)
-        self.devices.append(disk1)
-        self.devices.append(disk2)
-        self.devices.append(controller1)
-        self.devices.append(controller2)
-        self.devices.append(interface1)
-        self.devices.append(serial)
-        self.devices.append(console)
-        self.devices.append(channel)
-        self.devices.append(input)
-        self.devices.append(memballoon)
+
+        self.devices.append(self.emulator)
+        self.devices.append(self.disk1)
+        self.devices.append(self.disk2)
+        self.devices.append(self.controller1)
+        self.devices.append(self.controller2)
+        self.devices.append(self.interface1)
+        self.devices.append(self.serial)
+        self.devices.append(self.console)
+        self.devices.append(self.channel)
+        self.devices.append(self.input)
+        self.devices.append(self.memballoon)
+
+    def create_qcow2_tag(self):
+        self.disk1 = Element('disk', attrib={'type': 'file', 'device': 'disk'})
+        driver = Element('driver', attrib={'name': 'qemu', 'type': 'qcow2'})
+        source = Element('source', attrib={'file': self.boot})
+        target = Element('target', attrib={'dev': 'vda', 'bus': 'virtio'})
+        address = Element('address', attrib={'type': 'pci',
+                                             'domain': '0x0000',
+                                             'bus': '0x00',
+                                             'slot': '0x06',
+                                             'function': '0x0'})
+        self.disk1.append(driver)
+        self.disk1.append(source)
+        self.disk1.append(target)
+        self.disk1.append(address)
+
+    def create_iso_tag(self):
+        self.disk2 = Element('disk', attrib={'type': 'file', 'device': 'cdrom'})
+        driver = Element('driver', attrib={'name':'qemu', 'type': 'raw'})
+        source = Element('source', attrib={'file': self.cdrom})
+        target = Element('target', attrib={'bus': 'ide', 'dev': 'hdb'})
+        read_only = Element('readonly')
+        address = Element('address', attrib={'bus': '0',
+                                             'controller': '0',
+                                             'target': '0',
+                                             'type': 'drive',
+                                             'unit': '1'})
+        self.disk2.append(driver)
+        self.disk2.append(source)
+        self.disk2.append(target)
+        self.disk2.append(read_only)
+        self.disk2.append(address)
+
--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from create.vm import CreateVM
 import json
 
 create_test = CreateVM()
-create_test("test12", "/koko.img", "/wei.iso", 40)
+create_test("test2", "/koko.img", "/wei.iso", 256, 1)
 
 #test = DomainInfo()
 #print(json.dumps(test.show_domain_info_all()))
