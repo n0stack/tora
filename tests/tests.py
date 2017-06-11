04df18e8f5d8208a1214f80d2d4d8f1a15ffa8d0
palloc <palloc.mknet10@gmail.com>
2017-06-11 17:09:48 +0900

    add pool test

--- tests/tests.py
+++ tests/tests.py
@@ -2,12 +2,29 @@ import unittest
 import requests
 import json
 
+
 class TestGetApi(unittest.TestCase):
+    def __init__(self):
+        super().__init__()
+        self.base_url = 'http://localhost:5000'
+
     def test_vm(self):
-        response = requests.get('http://localhost:5000/vm')
+        response = requests.get(self.base_url+'/vm')
         self.assertEqual(response.status_code, 200)
         assert json.loads(response.text)
-        
+
+    def test_vm_name(self):
+        """
+        will
+        """
+        pass
+
+    def test_pool(self):
+        response = requests.get(self.base_url+'/pool')
+        self.assertEqual(response.status_code, 200)
+        assert json.loads(response.text)
+
+
     
 if __name__ == "__main__":
     unittest.main()
